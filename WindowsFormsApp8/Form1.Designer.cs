2fdc190a76749f84ca94559d07930f2b7d907fe6
venosu <57254839+venosu@users.noreply.github.com>
2021-03-31 10:58:29 +0200

    Add files via upload

---
+++ WindowsFormsApp8/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Auto Bunnyhop Loaded!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 46);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Auto Bunnyhop v1.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp8/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApp8
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("user32.dll")]
+
+        static extern void mouse_event(int a, int b, int c, int d, int e);
+
+        int Mouse3Down = 0x0020;
+        int Mouse3Up = 0x0040;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // cześć kirdzik, wiedziałem że tu zajrzysz :)
+
+        void AutoBhop()
+        {
+            while (true)
+            {
+                if (GetAsyncKeyState(Keys.Space)<0)
+                {
+                    mouse_event(Mouse3Down, 0, 0, 0, 0);
+                    Thread.Sleep(9);
+                    mouse_event(Mouse3Up, 0, 0, 0, 0);
+                    Thread.Sleep(9);
+                }
+                Thread.Sleep(10); //Thread.Sleep(5);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Thread BunnyHop = new Thread(AutoBhop) { IsBackground = true };
+            BunnyHop.Start();
+        }
+    }
+}
